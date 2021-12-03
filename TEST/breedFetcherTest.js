91a370bd51532de0c45699d85f4e71b0b76c3d1e
fathel mohamud <fathel.mohamud@gmail.com>
2021-12-03 01:35:29 -0500

    ok

--- TEST/breedFetcherTest.js
+++ TEST/breedFetcherTest.js
@@ -1,23 +1,25 @@
-let { fetchBreedDescripiton } = require('../breedFetcher');
-let {assert } = require('chai');
+const {fetchBreedDescripiton} = require('../breedFetcher');
+const {assert} = require('chai');
 
-describe ('fetchBreedDescription', () => {
-  if('returns a string description for a valid breed, via callback', (done) => {
-    fetchBreedDescripiton('Siberian', (err, desc) => {
-      assert.equal(err, null);
-      let expectedDesc ="The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
-      assert.equal(expectedDesc, desc.trim());
-      done();
-    });
-  });
+describe('fetchBreedDescription', () => {
+	if ('returns a string description for a valid breed, via callback', done => {
+		fetchBreedDescripiton('Siberian', (err, desc) => {
+			assert.equal(err, null);
+			const expectedDesc = 'The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.';
+			assert.equal(expectedDesc, desc.trim());
+			done();
+		});
+	}) {
 
-  it('return no description when an invalid breedName is passed', (done) => {
-  fetchBreedDescripiton('Fathel', (err, desc) => {
-    assert.equal(err, null);
-    let expectedDesc = null;
-    assert.equal(expectedDesc, desc);
-    done();
-  });
-});
+	}
+
+	it('return no description when an invalid breedName is passed', done => {
+		fetchBreedDescripiton('Fathel', (err, desc) => {
+			assert.equal(err, null);
+			const expectedDesc = null;
+			assert.equal(expectedDesc, desc);
+			done();
+		});
+	});
 });
 
